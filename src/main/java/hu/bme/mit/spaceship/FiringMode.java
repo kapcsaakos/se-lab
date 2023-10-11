d98829d9110ba5f1ba11edde4ccfd37129f3ff69
Kapcsa Akos <akoskapcsa@gmail.com>
2023-10-11 14:43:03 +0200

    Fix firingSuccess

--- src/main/java/hu/bme/mit/spaceship/FiringMode.java
+++ src/main/java/hu/bme/mit/spaceship/FiringMode.java
@@ -4,11 +4,8 @@ package hu.bme.mit.spaceship;
 * Weapon firing mode enumeration
 *
 */
-<<<<<<< HEAD
 //modified in branch-A
-=======
 //modified in branch-B
->>>>>>> branch-B
 public enum FiringMode {
   SINGLE, ALL
 }

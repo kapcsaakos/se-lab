272b7c1044c23be81456b2bf748c3b5b623f6e2f
Kapcsa Akos <akoskapcsa@gmail.com>
2023-10-11 15:38:20 +0200

    fix TorpedoStore n3 is cool

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,12 +30,13 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
 //a
     // simulate random overheating of the launcher bay which prevents firing
+    // is cool man
     Random generator = new Random();
     double r = generator.nextDouble();
 
